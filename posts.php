238821e6420d06b9389abd64aa6d802ae1e35b4d
DemeRTT <kornev.dimchik04@mail.ru>
2024-04-29 06:32:50 +0300

    add posts.php

---
+++ posts.php
@@ -0,0 +1,34 @@
+<?php
+$link = mysqli_connect('127.0.0.1', 'root', 'kali', 'first_db');
+$id = $_GET['id'];
+$sql = "SELECT * FROM posts WHERE id=$id";
+$res = mysqli_query($link, $sql);
+$rows = mysqli_fetch_array($res);
+$title = $rows['title'];
+$main_text = $rows['main_text'];
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-widht, initial-scale=1.0">
+    <title>Корнев Д.А.</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel=”stylesheet” href=”https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css” />
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jersey+10&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <?php
+    echo "<h1> $title </h1>";
+    echo "<p> $main_text </p>";
+    ?>
+</body>
+
+</html>
